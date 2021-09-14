d0daa3e4ad2e8e3a821fae6868e955b4dc5212b8
Krzysztof <krzysiek.swol@gmail.com>
2021-09-14 23:12:58 +0200

    add post service

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {objectKeys} from 'codelyzer/util/objectKeys';
+
 
 
 
@@ -12,6 +14,9 @@ export class DataService {
   public aircraftType = '';
   private url = 'https://flightcarrier.free.beeceptor.com/flightcarrier';
   private urlSeat = 'https://flightcarrier.free.beeceptor.com/airplane';
+  private urlReservation = 'https://flightcarrier.proxy.beeceptor.com/reservation';
+  dataSend: {};
+  private json: Object;
   constructor(private http: HttpClient) {}
   get(): Observable<any> {
     return this.http.get(this.url);
@@ -19,5 +24,8 @@ export class DataService {
   getSeats(): Observable<any> {
     return this.http.get(this.urlSeat,
       {params: new HttpParams().set('plane', this.aircraftType)});
+  }
+  post(): any {
+    this.http.post(this.urlReservation, this.dataSend ).subscribe(data => alert(Object.values(data)));
   }
 }
--- src/app/summary-page/summary-page.component.ts
+++ src/app/summary-page/summary-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {DataShareService} from '../service/data-share.service';
 import {TicketReservation} from '../dataModel/ticketReservationModel';
 import {LocalStorageService} from '../service/local-storage.service';
+import {DataService} from '../service/data.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-summary-page',
@@ -21,11 +23,15 @@ export class SummaryPageComponent {
     flightStartDate: this.dataShareService.ticketSummary.flightStartDate,
     seatNumber: this.dataShareService.ticketSummary.seatNumber
 };
-  constructor(private dataShareService: DataShareService, private localStorage: LocalStorageService) { }
+  constructor(private dataShareService: DataShareService,
+              private localStorage: LocalStorageService,
+              private http: DataService) { }
   setDataForLocalStorage(): number {
     return this.localStorage.dataItems.push(this.reservation);
   }
   setLocalStorage(): void {
+    this.http.dataSend = this.reservation;
+    this.http.post();
     this.localStorage.setLocalStorage();
   }
 }
